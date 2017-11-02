08447300819bd62e968a8108f7b842b7b4fab43a
camel8899 <camel8899@gmail.com>
2017-11-02 13:25:03 +0000

    dht11

--- Lab_Bluetooth/gattlib_ex.py
+++
@@ -1,6 +0,0 @@
-from gattlib import DiscoveryService
-
-service  = DiscoveryService()
-devices = service.discover(10)
-for add, name in devices.items():
-    print("name: {}, address: {}".format(name,add))
